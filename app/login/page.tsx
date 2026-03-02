d0e77013277165f1b4ed0aaa91d0f2fac6661d9b
Meatclump <hepe22@hotmail.com>
2026-03-02 11:52:51 +0100

    login setup

---
+++ app/login/page.tsx
@@ -0,0 +1,21 @@
+import { LoginForm } from "@/components/login-form"
+import Link from "next/link"
+import { Button } from "@/components/ui/button"
+import { FaTurnUp } from "react-icons/fa6"
+
+export default function LoginPage() {
+  return (
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <Button variant={"link"} className="text-muted-foreground absolute top-0 left-0" asChild>
+        <Link href={"/"}>
+          <FaTurnUp className="rotate-270" />
+          Go Back Home
+        </Link>
+      </Button>
+      <div className="flex w-full max-w-sm flex-col gap-6">
+          <h1 className="font-roboto-slab font-xl text-center font-extrabold text-red-700 text-2xl">BBList</h1>
+        <LoginForm />
+      </div>
+    </div>
+  )
+}
