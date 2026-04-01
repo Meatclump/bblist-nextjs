63122f652403d3a20efb89eca7564302affe3da5
Meatclump <hepe22@hotmail.com>
2026-04-01 10:58:54 +0200

    Fix bandaid id assignment for Positions

--- app/manage/page.tsx
+++ app/manage/page.tsx
@@ -1,18 +1,15 @@
 import Models from "@/components/teams/model/models";
 import Positions from "@/components/teams/position/positions";
 import Teams from "@/components/teams/team/teams";
-import TeamModels from "@/components/teams/teamModels/teamModels";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { getModels } from "@/server/models";
 import { getPositions } from "@/server/positions";
-import { getData as getTeamModels } from "@/server/teamModels";
 import { getTeams } from "@/server/teams";
 
 export default async function Manage() {
     const teams = await getTeams()
     const positions = await getPositions()
     const models = await getModels()
-    const teamModels = await getTeamModels()
     return (
         <div className="flex w-full justify-center">
             <main className="flex flex-col gap-3 p-3 w-full max-w-6xl">
